62fd7d230b4c8665abbf8a538a7dbcf4375310fa
KitchenBoy <coleton.trenton@outlook.com>
2024-01-10 19:43:31 -0600

    Implement RiptideLobby
    I'm still not entirely sure about how I'm gonna do this, but it's fiiiiine

---
+++ Core/src/Network/Riptide/Layer/Lobby/RiptideLobby.cs
@@ -0,0 +1,65 @@
+﻿using LabFusion.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabFusion.Riptide
+{
+    public class RiptideLobby : NetworkLobby
+    {
+        public List<KeyValuePair<string, string>> Metadata = new List<KeyValuePair<string, string>>();
+
+        public RiptideLobby(List<KeyValuePair<string, string>> metadata)
+        {
+            Metadata = metadata;
+        }
+
+        public override Action CreateJoinDelegate(ulong lobbyId)
+        {
+            return () => ClientManagement.JoinPublicLobby((ushort)lobbyId);
+        }
+
+        public override string GetMetadata(string key)
+        {
+            foreach (var pair in Metadata)
+            {
+                if (pair.Key == key)
+                    return pair.Value;
+            }
+
+            return string.Empty;
+        }
+
+        public override void SetMetadata(string key, string value)
+        {
+            KeyValuePair<string, string> keyPair = new KeyValuePair<string, string>(key, value);
+
+            foreach (var pair in Metadata)
+            {
+                if (pair.Key == key)
+                {
+                    Metadata.Remove(pair);
+                }
+            }
+
+            Metadata.Add(keyPair);
+        }
+
+        public override bool TryGetMetadata(string key, out string value)
+        {
+            foreach (var pair in Metadata)
+            {
+                if (pair.Key == key)
+                {
+                    value = pair.Value;
+                    return true;
+                }
+            }
+
+            value = string.Empty;
+            return false;
+        }
+    }
+}
